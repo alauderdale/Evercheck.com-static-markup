cdff70511e6a99e515a3e49ed3c8e9ec7c17ce4f
Alex Lauderdale <alauderdale@mac.com>
2020-01-08 12:45:06 -0700

    home

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -1,5 +1,36 @@
 $(document).ready(function(){
 
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+      // slidesOffsetAfter: 100,
+
+      loop: true,
+      keyboard: true,
+      initialSlide: 0,
+      slidesPerView: 'auto',
+      loopedSlides: 6,
+
+
+
+    // If we need pagination
+    pagination: {
+      clickable: true,
+      el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+
+    });
+
+
+
 
   $( ".menu-button" ).click(function() {
         $( "html" ).toggleClass( "nav-open" );
